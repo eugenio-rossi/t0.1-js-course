e779f65a0e45061ebc67fa6f9ab253409625d411
eugenio-rossi <eugeniotrossi@gmail.com>
2021-02-28 18:07:06 +0100

    Done

--- custom.js
+++ custom.js
@@ -1,20 +1,36 @@
 console.log('connected');
+
 const spinner = document.getElementById('spinner');
+const postResult = document.getElementById('postResult');
+const postEndpoint = 'https://my-json-server.typicode.com/typicode/demo/posts'; 
+
 spinner.classList.add('visually-hidden');
 
-function onclickFunction(){
-    let urlValue = document.getElementById("url").value;
-    spinner.classList.remove('visually-hidden');
+// function onclickFunction(){
+//     let urlValue = document.getElementById("url").value;
+//     spinner.classList.remove('visually-hidden');
 
-    fetch(urlValue)
-      .then(res => res.status)
-      .then(data => {
-          alert(data);
-      })
-      .catch(fail => {
-          alert(`Porca madò: ${fail}`);
-      })
-      .finally(() => 
-          spinner.classList.add('visually-hidden')
-      )      
-};
+//     fetch(urlValue)
+//       .then(res => res.status)
+//       .then(data => {
+//           alert(data);
+//       })
+//       .catch(fail => {
+//           alert(`Porca madò: ${fail}`);
+//       })
+//       .finally(() => 
+//           spinner.classList.add('visually-hidden')
+//       )      
+// };
+
+function onclickLoad(){
+    fetch(postEndpoint)
+        .then(res => res.json())
+        .then(data => {
+            let variableAppodjAskedByCrocco = "";
+            data.forEach(element => {
+                variableAppodjAskedByCrocco += `<li>${element.title}</li>`;
+            });
+            postResult.innerHTML = `<ul>${variableAppodjAskedByCrocco}</ul>`;
+        })
+}
